fe7576db9e3f49337a37c2f326abc24b392e3e44
clarecorr <clare.e.corrigan@gmail.com>
2017-07-28 10:39:25 -0400

    added classes

---
+++ MVCReviewSite/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+{
+    public class Category
+    {
+        [Key] //Primary Key
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
